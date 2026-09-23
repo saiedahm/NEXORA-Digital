444a0d97f3965073c25394688f9a555dcaad6d09
saiedahm <daralfan78@gmail.com>
2026-09-23 13:22:35 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,16 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "NEXORA DIGITAL",
-  description: "Your Business. Built. Managed. Grown. By AI.",
-  manifest: "/manifest.webmanifest"
+  description:
+    "Your Business. Built. Managed. Grown. By AI.",
+  manifest: "/manifest.webmanifest",
 };
 
 export default function RootLayout({
-  children
-}: {
+  children,
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="de">
       <body>{children}</body>
