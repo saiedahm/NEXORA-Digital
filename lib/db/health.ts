cf5103ebe4acf8f2463dc654816572e32aad9bb8
saiedahm <daralfan78@gmail.com>
2026-09-23 13:56:57 +0200

    Create health.ts

---
+++ lib/db/health.ts
@@ -0,0 +1,10 @@
+ import { prisma } from "@/lib/db/client";
+
+export async function checkDatabaseHealth(): Promise<boolean> {
+  try {
+    await prisma.$queryRaw`SELECT 1`;
+    return true;
+  } catch {
+    return false;
+  }
+}
