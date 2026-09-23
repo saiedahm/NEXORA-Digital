812fbf73b77dd602427db94dad6e68737b65df50
saiedahm <daralfan78@gmail.com>
2026-09-23 12:31:08 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { BrandLogo } from "@/components/brand/BrandLogo";
+
+const agents = [
+  "Alex",
+  "Nora",
+  "Leo",
+  "Mia",
+  "Lina",
+  "Aria",
+  "Eli",
+  "Zoe",
+  "Max",
+  "Kai",
+  "Iris",
+  "Adam",
+  "Ryan",
+  "Luna",
+  "Atlas"
+]; 
