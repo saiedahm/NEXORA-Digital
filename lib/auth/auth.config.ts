57fbd463a0cd8ea83e11f9b15da00be6827b170b
saiedahm <daralfan78@gmail.com>
2026-09-26 18:40:09 +0200

    Fix Edge middleware bundle by separating Prisma-free NextAuth config

---
+++ lib/auth/auth.config.ts
@@ -0,0 +1,19 @@
+import Google from "next-auth/providers/google";
+import Facebook from "next-auth/providers/facebook";
+import type { NextAuthConfig } from "next-auth";
+
+export default {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    Facebook({
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+} satisfies NextAuthConfig;
