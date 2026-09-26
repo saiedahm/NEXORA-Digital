d3946ac9e2d1b1712f763f2a35586fe3ee44848f
saiedahm <daralfan78@gmail.com>
2026-09-26 11:28:58 +0200

    Update client.ts

--- lib/db/client.ts
+++ lib/db/client.ts
@@ -1,17 +1,20 @@
  import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma?: PrismaClient;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
+function createPrismaClient() {
+  return new PrismaClient({
     log:
       process.env.NODE_ENV === "development"
         ? ["error", "warn"]
         : ["error"],
   });
+}
+
+export const prisma =
+  globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
