ec5a0978be2482b495ff8fd896513ecc4f720ef0
saiedahm <daralfan78@gmail.com>
2026-09-26 20:16:19 +0200

    Enable clean section navigation without URL hashes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { HomeClient } from "@/components/home/HomeClient";
+import { HashCleaner } from "@/components/navigation/HashCleaner";
 
 export default function Home() {
-  return <HomeClient />;
+  return (
+    <>
+      <HashCleaner />
+      <HomeClient />
+    </>
+  );
 }
